bec77cdeb878c3b4c4c0a39850db2f877f7630bb
zhuxi <732632754@qq.com>
2019-06-19 14:00:20 +0800

    Initial Commit

---
+++ XVPN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XVPN
+//
+//  Created by 哔哩哔哩 on 2019/6/19.
+//  Copyright © 2019 xizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XVPNTests/XVPNTests.swift
@@ -0,0 +1,34 @@
+//
+//  XVPNTests.swift
+//  XVPNTests
+//
+//  Created by 哔哩哔哩 on 2019/6/19.
+//  Copyright © 2019 xizi. All rights reserved.
+//
+
+import XCTest
+@testable import XVPN
+
+class XVPNTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
